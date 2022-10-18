23f7203dd0d07a6c53a407ff9bad4ff8ae284d4c
Allen Pham <Allendpham@gmail.com>
2022-10-18 15:19:42 -0500

    float avgRating try four

--- frontend/src/components/SpotShow/index.js
+++ frontend/src/components/SpotShow/index.js
@@ -32,7 +32,7 @@ const SpotShow = () => {
    let ratingArr = singleSpot?.avgStarRating?.toString().split('');
 
    if(singleSpot?.avgStarRating === 0) displayRating = "No Current Reviews";
-   else if(Number.isInteger(singleSpot?.avgStarRating)) displayRating = `${Number(singleSpot?.avgStarRating)}.0`;
+   else if(Number.isInteger(Number(singleSpot?.avgStarRating))) displayRating = `${Number(singleSpot?.avgStarRating)}.0`;
    else if(ratingArr.slice(2).length === 1) displayRating = singleSpot?.avgStarRating;
    else displayRating = parseFloat(singleSpot?.avgStarRating).toFixed(2);
 
